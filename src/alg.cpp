9d98923591208f0f5d1ddbb462c037972cdc9ce5
Saltykov21ist2 <99250746+Saltykov21ist2@users.noreply.github.com>
2022-03-17 15:05:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,24 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int q = 0, p = 0, w = size - 1;
+while (p < w) {
+int mm = (p + w) / 2;
+if (arr[mm] == value) {
+q++;
+int tt = mm;
+while (arr[--mm] == value) {
+q++;
+}
+while (arr[++tt] == value) {
+q++;
+}
+return q;
+} else if (arr[mm] > value) {
+w = mm;
+} else {
+p = mm + 1;
+}
+}
+return 0;
 }
